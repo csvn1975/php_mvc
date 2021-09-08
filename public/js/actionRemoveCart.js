5619f36afe583a300774981f56304046295eb2a0
Nguyen Le <csvn1975khonnan@gmail.com>
2021-09-08 14:00:35 +0200

    add use strict

--- public/js/actionRemoveCart.js
+++ public/js/actionRemoveCart.js
@@ -1,45 +1,47 @@
-'use strict';
+"use strict";
 
 function actionRemove(event) {
     event.preventDefault();
 
-    var parentRow = event.target.closest('tr')
-    var linkElement = parentRow.querySelector('.cart_quantity_delete')
+    var parentRow = event.target.closest("tr");
+    var linkElement = parentRow.querySelector(".cart_quantity_delete");
 
-    var url = linkElement.dataset['url']
-    var product_id = linkElement.dataset['id']
-    
-    var formData = new FormData()
-    formData.append('id', product_id)
-     
-    fetch( url , {
-        method: 'POST', 
+    var url = linkElement.dataset["url"];
+    var product_id = linkElement.dataset["id"];
+
+    var formData = new FormData();
+    formData.append("id", product_id);
+
+    fetch(url, {
+        method: "POST",
         body: formData,
     })
-    .then(response => {
-        return response.clone().json();
-    })
+        .then((response) => {
+            return response.clone().json();
+        })
 
-    .then(data => {
-        if (data.status == 200) {
-            var cartElement = document.getElementById("cart-items-count");
-            parentRow.remove();
-            cartElement.innerText = data.cartItemsCount;
+        .then((data) => {
+            if (data.status == 200) {
+                var cartElement = document.getElementById("cart-items-count");
+                parentRow.remove();
+                cartElement.innerText = data.cartItemsCount;
 
-            document.querySelector(".sub-total-price").innerText  = data.subtotalPrice;
-            document.querySelector(".tax-total-price").innerText  = data.totalTaxPrice;
-            document.querySelector(".total-price").innerText  = data.totalPrice; 
-        }
+                document.querySelector(".sub-total-price").innerText =
+                    data.subtotalPrice;
+                document.querySelector(".tax-total-price").innerText =
+                    data.totalTaxPrice;
+                document.querySelector(".total-price").innerText =
+                    data.totalPrice;
+            }
 
-        alert(data.message)
-    })
-    .catch((error) => {
-        alert(data.message)
-    });
+            alert(data.message);
+        })
+        .catch((error) => {
+            alert(data.message);
+        });
 }
 
-
-const deleteCheckboxes = document.querySelectorAll('.cart_delete');
-    for (const deleteCheckbox of deleteCheckboxes) {
-        deleteCheckbox.addEventListener('click', actionRemove) 
-    }
+const deleteCheckboxes = document.querySelectorAll(".cart_delete");
+for (const deleteCheckbox of deleteCheckboxes) {
+    deleteCheckbox.addEventListener("click", actionRemove);
+}
